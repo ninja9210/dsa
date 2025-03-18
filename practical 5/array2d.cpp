f184276fb665f084eed08e776c3d0b650f0e322a
ninja9210 <pp506345@outlook.com>
2025-03-18 22:37:45 +0530

    Update array2d.cpp

--- practical 5/array2d.cpp
+++ practical 5/array2d.cpp
@@ -1,26 +1,25 @@
 #include <iostream.h>
 #include<conio.h>
 using namespace std;
-// Function to find the intersection of two arrays
+
 void FindIntersection(int arr1[], int arr2[], int temp[],
                       int m, int n, int &k)
 {
     int i = 0, j = 0;
-    // vector to store the intersection of the arr1[] and
-    // arr2[]
+    
     while (i < m && j < n)
     {
-        // ith element can not be common element
+        
         if (arr1[i] < arr2[j])
         {
             i++;
         }
-        // jth element can not be common element
+        
         else if (arr2[j] < arr1[i])
         {
             j++;
         }
-        // if arr1[i] == arr2[j]
+       
         else
         {
             temp[k] = arr1[i];
@@ -39,18 +38,16 @@ int main()
     int n1 = sizeof(arr1) / sizeof(arr1[0]);
     int n2 = sizeof(arr2) / sizeof(arr2[0]);
     int n3 = sizeof(arr3) / sizeof(arr3[0]);
-    // temp array to store the common elements of arr1 and
-    // arr2 arrays
+    
     int temp[200000];
-    // ans array to store the common elements of temp and
-    // arr3 arrays (i.e common elements of all 3 arrays)
+    
     int ans[200000];
     int k = 0;
-    // function call to find the temp array
+   
     FindIntersection(arr1, arr2, temp, n1, n2, k);
     int tempSize = k;
     k = 0;
-    // function call to find the ans array.
+
     FindIntersection(arr3, temp, ans, n3, tempSize, k);
     cout << "Common elements present in arrays are : \n";
     for (int i = 0; i < k; i++)
