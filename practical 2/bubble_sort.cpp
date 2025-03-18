35f75a2e417b1c907589328771dc997f45cf0083
ninja9210 <pp506345@outlook.com>
2025-03-18 22:35:32 +0530

    Update bubble_sort.cpp

--- practical 2/bubble_sort.cpp
+++ practical 2/bubble_sort.cpp
@@ -5,13 +5,13 @@ void main()
     clrscr();
     int a[10] = {25, 18, 6, 2, 8, 12, 23, 55, 34, 41};
     int i, j, t;
-    // print array elements before sorting
+    
     cout << "\nArray Elements Before Sorting are:\n";
     for (i = 0; i < 10; i++)
     {
         cout << a[i] << "\t";
     }
-    // sort the array using bubble sort
+   
     for (i = 0; i < 10; i++)
     {
         for (j = 0; j < 10; j++)
@@ -24,7 +24,7 @@ void main()
             }
         }
     }
-    // print the sorted list
+  
     cout << "\nArray Elements after Sorting are:\n";
     for (i = 0; i < 10; i++)
     {
